47951089dd36b92205ad37102990b938894d4ed6
Joey <786678257@qq.com>
2019-09-15 16:25:22 +0800

    use cdn

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,17 @@
 module.exports = {
   outputDir: 'build',
   publicPath: './',
-};
+  chainWebpack: (config) => {
+    // 最小化处理: 代码的最优化
+    config.optimization.minimize(true);
+    // 分割代码
+    config.optimization.splitChunks({
+        chunks: 'all',
+    });
+    // 忽略这些模块的打包
+    config.externals({
+        vue: 'Vue',
+        'element-ui': 'ELEMENT',
+    });
+  },
+}
